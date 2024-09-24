245816afdb6137ba9f178598fb39a3bbb8e496cc
vandeefeng <naoyanzifzm@hotmail.com>
2024-09-24 20:54:53 +0800

    Vandee - 2024-09-24T20:54:53+0800

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,8 +2,11 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps {
+  placeholder?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
